4813201fa5887fab02f5e25382302dab1a89a0fc
engelsprado7 <99674354+engelsprado7@users.noreply.github.com>
2024-07-03 21:33:46 -0600

    refactor: Update main wrapper gap, tagline, and flex-flow in index page

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    colors: {
+      'accent-regular': 'var(--accent-regular)',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
