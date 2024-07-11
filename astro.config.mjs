635b4d944c4cc8d81aef6626ce7df5e19ae9fd58
engelsprado7 <99674354+engelsprado7@users.noreply.github.com>
2024-07-11 11:20:55 -0600

    refactor: Update dependencies and integrate @astrojs/sitemap for SEO improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,25 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
+import path from 'path';
+import { fileURLToPath } from 'url';
+import astrowind from './vendor/integration';
 import sitemap from "@astrojs/sitemap";
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://master--engelsdev.netlify.app/',
-  integrations: [tailwind(), mdx(), sitemap()]
+  integrations: [tailwind(), mdx(), sitemap(),
+  astrowind({
+    config: './src/config.yaml',
+  }),
+  ],
+
+  vite: {
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './src'),
+      },
+    },
+  },
 });
---
+++ src/types.d.ts
@@ -0,0 +1,51 @@
+import type { AstroComponentFactory } from "astro/runtime/server/index.js";
+import type { HTMLAttributes, ImageMetadata } from "astro/types";
+
+export interface Taxonomy {
+  slug: string;
+  title: string;
+}
+
+export interface MetaData {
+  title?: string;
+  ignoreTitleTemplate?: boolean;
+
+  canonical?: string;
+
+  robots?: MetaDataRobots;
+
+  description?: string;
+
+  openGraph?: MetaDataOpenGraph;
+  twitter?: MetaDataTwitter;
+}
+
+export interface MetaDataRobots {
+  index?: boolean;
+  follow?: boolean;
+}
+
+export interface MetaDataImage {
+  url: string;
+  width?: number;
+  height?: number;
+}
+
+export interface MetaDataOpenGraph {
+  url?: string;
+  siteName?: string;
+  images?: Array<MetaDataImage>;
+  locale?: string;
+  type?: string;
+}
+
+export interface MetaDataTwitter {
+  handle?: string;
+  site?: string;
+  cardType?: string;
+}
+
+interface Social {
+  icon?: string;
+  href?: string;
+}
