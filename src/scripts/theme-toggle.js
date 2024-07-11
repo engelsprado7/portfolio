92444c12c2a8f53a443ee11aeef76a1eb1efd373
engelsprado7 <99674354+engelsprado7@users.noreply.github.com>
2024-07-11 15:57:52 -0600

    refactor: Update dependencies and integrate @astrojs/sitemap for SEO improvements

---
+++ src/scripts/theme-toggle.js
@@ -0,0 +1,34 @@
+class ThemeToggle extends HTMLElement {
+    constructor() {
+        super();
+
+        const button = this.querySelector("button");
+
+        /** Set the theme to dark/light mode. */
+        const setTheme = (dark = false) => {
+            document.startViewTransition(() => {
+                document.documentElement.classList[dark ? "add" : "remove"](
+                    "theme-dark"
+                );
+                button.setAttribute("aria-pressed", String(dark));
+
+            })
+
+        };
+
+        // Toggle the theme when a user clicks the button.
+        button.addEventListener("click", () => setTheme(!this.isDark()));
+
+        if (!document.startViewTransition)
+            setTheme(!this.isDark())
+
+        // Initialize button state to reflect current theme.
+        document.startViewTransition(setTheme);
+    }
+
+    isDark() {
+        return document.documentElement.classList.contains("theme-dark");
+    }
+}
+customElements.define("theme-toggle", ThemeToggle);
+console.log("ThemeToggle loaded");
