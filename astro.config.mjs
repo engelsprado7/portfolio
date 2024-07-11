76fa4d33c37b50a2caa29f9eaeb551ba895bf9bc
engelsprado7 <99674354+engelsprado7@users.noreply.github.com>
2024-07-11 09:01:22 -0600

    refactor: Update dependencies and integrate @astrojs/sitemap for SEO improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  site: 'https://master--engelsdev.netlify.app/',
+  integrations: [tailwind(), mdx(), sitemap()]
 });
